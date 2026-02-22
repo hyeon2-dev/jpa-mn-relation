74a64cfd30be326dd1234c8fedf6ba45eab32569
hyeon2-dev <ski069182@gmail.com>
2026-02-22 18:33:42 +0900

    first commit

---
+++ src/main/java/com/example/jpamnrelation/JpaMnRelationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamnrelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMnRelationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMnRelationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpamnrelation/JpaMnRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamnrelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMnRelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
